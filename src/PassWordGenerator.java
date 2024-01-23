ff05ca4166406de8d7f0078678cab5fcdf7c184a
vishal <vishal88kh@gmail.com>
2024-01-23 21:45:40 +0530

    added frame , panel and button

--- src/PassWordGenerator.java
+++ src/PassWordGenerator.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
 public class PassWordGenerator {
+    private JTextField lengthField;
+
+    public PassWordGenerator() {
+        JFrame jFrame = new JFrame("Password Generator");
+        JPanel jpanel = new JPanel();
+        lengthField = new JTextField(10);
+        JTextField passwordGenerated = new JTextField(10);
+        JButton jButton = new JButton();
+
+    }
 }
